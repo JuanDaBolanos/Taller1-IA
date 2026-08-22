9da2bca9923eea632da20cd83e46d403bea4b705
iv-vargas <i.vargasr2@uniandes.edu.co>
2026-08-22 11:37:47 -0500

    Manhattan

--- EstacionEspacial/algorithms/heuristics.py
+++ EstacionEspacial/algorithms/heuristics.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 from algorithms import utils
 from algorithms.problems import SystemRepairProblem
-
+import math
 
 def nullHeuristic(state, problem=None):
     """
@@ -21,8 +21,33 @@ def manhattanHeuristic(state, problem):
     - the nearest pending T if the robot has the kit and systems remain.
     - C if all systems have been repaired.
     """
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+    hasKit = state[2]
+    if not hasKit:
+        posKit = problem.kitPosition
+        x_cordinate = posKit[0]
+        y_cordinate = posKit[1]
+    
+    elif  len(problem.systemPositions) == 0:
+        control_pos = problem.controlPosition
+        x_cordinate = control_pos[0]
+        y_cordinate = control_pos[1]
+    else:
+        smallest_manhattan = math.inf
+        for cord in state[2]:
+            x_cordinate = cord[0]
+            y_cordinate = cord[1]
+            
+            new_manhattan = abs(state[0][0] - x_cordinate) + abs(state[0][1] - y_cordinate)
+            if (new_manhattan<smallest_manhattan):
+                smallest_manhattan = new_manhattan
+        return smallest_manhattan
+            
+    
+
+
+
+    return abs(state[0][0] - x_cordinate) + abs(state[0][1] - y_cordinate)
+    #utils.raiseNotDefined()
 
 
 def euclideanHeuristic(state, problem):
